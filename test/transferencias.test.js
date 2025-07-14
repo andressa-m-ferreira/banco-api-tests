999636167e95c677b860de0b3cb5440fa32ad655
Andressa M F Silva <fsilvaandressa@gmail.com>
2025-07-14 19:26:55 -0300

    Adding the library .env for global variables

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { expect } = require ('chai');
+require('dotenv').config()
 
 describe('Tranferências', () => {
     describe('POST /transferencias', () => {
@@ -15,7 +16,7 @@ describe('Tranferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
@@ -33,7 +34,7 @@ describe('Tranferências', () => {
         })
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async () =>{
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
